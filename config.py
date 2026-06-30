142c637f18b0a34dcefd00448428338b0041faad
Prince kumar <princekumarsum@gmail.com>
2026-06-30 19:33:20 +0530

    Add default SECRET_KEY to Config class
    Set a default value for SECRET_KEY if not provided.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "prionix-secret-key"
 
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
