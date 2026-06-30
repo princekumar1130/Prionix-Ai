45d14fb7be5679c93390fdee217e76d056292df2
Prince kumar <princekumarsum@gmail.com>
2026-06-30 14:47:58 +0530

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ Description:
 {lead.description}
 """
 
-        mail.send(msg)
+        #mail.send(msg)
 
         client_msg = Message(
             subject="Thank You For Contacting Prionix AI",
@@ -105,7 +105,7 @@ Regards,
 Prionix AI Team
 """
 
-        mail.send(client_msg)
+        #mail.send(client_msg)
         flash(
     "✅ Thank you for contacting Prionix AI. We have received your request and will contact you shortly.",
     "success"
