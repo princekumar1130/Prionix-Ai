12427b47dd25cd95e731971643ba7ec9dc110bf3
Prince kumar <princekumarsum@gmail.com>
2026-06-30 21:00:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
 import os
 
 class Config:
+    SECRET_KEY=os.environ.get("SECRET_KEY","prionix-secret-key")
+    SQLALCHEMY_DATABASE_URI="sqlite:///db.sqlite3"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "prionix-secret-key"
+    BREVO_API_KEY=os.environ.get("BREVO_API_KEY")
+    BREVO_SENDER_EMAIL=os.environ.get("BREVO_SENDER_EMAIL","prionixai@gmail.com")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "True") == "True"
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
+    ADMIN_USERNAME="Princekumar_1130"
+    ADMIN_PASSWORD="Prionixai12!@"
 
     ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME")
     ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
